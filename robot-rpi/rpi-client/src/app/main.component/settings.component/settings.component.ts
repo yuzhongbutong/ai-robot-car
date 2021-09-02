f125ce9f14e4faff09fa34b6a2a0599088d1ad52
Joey <zhaojingyu126@126.com>
2021-09-02 08:23:21 +0800

    update workspace.

--- robot-rpi/rpi-client/src/app/main.component/settings.component/settings.component.ts
+++ robot-rpi/rpi-client/src/app/main.component/settings.component/settings.component.ts
@@ -10,22 +10,7 @@ import { Constant } from 'src/utils/constant';
 })
 export class SettingsComponent implements OnInit {
 
-  opened = true;
-  modeNum = 0;
-  positionNum = 0;
-  dock = false;
-  closeOnClickOutside = false;
-  closeOnClickBackdrop = false;
-  showBackdrop = false;
-  animate = true;
-  trapFocus = true;
-  autoFocus = true;
-  keyClose = false;
-  autoCollapseHeight = 0;
-  autoCollapseWidth = 0;
-
-  MODES: Array<string> = ['over', 'push', 'slide'];
-  POSITIONS: Array<string> = ['left', 'right', 'top', 'bottom'];
+  tapActiveIndex = 0;
 
   constructor(private http: HttpClient) { }
 
@@ -36,89 +21,5 @@ export class SettingsComponent implements OnInit {
     // this.http.post(Constant.API.SAVE_SETTINGS, {'mqtt-local': {host: '192.168.3.12', port: 1883}}).pipe(map((response: any) => {
     //   console.log(response)
     // })).subscribe();
-  }
-
-  toggleOpened(): void {
-    this.opened = !this.opened;
-  }
-
-  toggleMode(): void {
-    this.modeNum++;
-
-    if (this.modeNum === this.MODES.length) {
-      this.modeNum = 0;
-    }
-  }
-
-  toggleAutoCollapseHeight(): void {
-    this.autoCollapseHeight = this.autoCollapseHeight ? 0 : 500;
-  }
-
-  toggleAutoCollapseWidth(): void {
-    this.autoCollapseWidth = this.autoCollapseWidth ? 0 : 500;
-  }
-
-  togglePosition(): void {
-    this.positionNum++;
-
-    if (this.positionNum === this.POSITIONS.length) {
-      this.positionNum = 0;
-    }
-  }
-
-  toggleDock(): void {
-    this.dock = !this.dock;
-  }
-
-  toggleCloseOnClickOutside(): void {
-    this.closeOnClickOutside = !this.closeOnClickOutside;
-  }
-
-  toggleCloseOnClickBackdrop(): void {
-    this.closeOnClickBackdrop = !this.closeOnClickBackdrop;
-  }
-
-  toggleShowBackdrop(): void {
-    this.showBackdrop = !this.showBackdrop;
-  }
-
-  toggleAnimate(): void {
-    this.animate = !this.animate;
-  }
-
-  toggleTrapFocus(): void {
-    this.trapFocus = !this.trapFocus;
-  }
-
-  toggleAutoFocus(): void {
-    this.autoFocus = !this.autoFocus;
-  }
-
-  toggleKeyClose(): void {
-    this.keyClose = !this.keyClose;
-  }
-
-  onOpenStart(): void {
-    console.info('Sidebar opening');
-  }
-
-  onOpened(): void {
-    console.info('Sidebar opened');
-  }
-
-  onCloseStart(): void {
-    console.info('Sidebar closing');
-  }
-
-  onClosed(): void {
-    console.info('Sidebar closed');
-  }
-
-  onTransitionEnd(): void {
-    console.info('Transition ended');
-  }
-
-  onBackdropClicked(): void {
-    console.info('Backdrop clicked');
   }
 }

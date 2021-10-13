24bdfc7c4c9fa24c1f32ebb818e0369fdb3edb06
Joey <zhaojingyu126@126.com>
2021-10-13 20:55:47 +0800

    update env.

--- robot-rpi/src/service/login_service.py
+++ robot-rpi/src/service/login_service.py
@@ -1,6 +1,5 @@
-
-# from crypt import crypt
 from hmac import compare_digest as compare_hash
+from os import getenv
 from itsdangerous import TimedJSONWebSignatureSerializer as Serializer
 from src.config import settings
 from src.utils.utils import run_command
@@ -14,12 +13,15 @@ class LoginService:
         return settings.TOKEN_SCHEME + ' ' + token
 
     def verify_auth(self, username, cleartext):
-        # result = run_command('sudo cat /etc/shadow | grep -w ' + username)
-        # if result:
-        #     cryptedpasswd = result.split(':')[1]
-        #     is_correct = compare_hash(crypt(
-        #         cleartext, cryptedpasswd), cryptedpasswd)
-        #     if is_correct:
-                return True
-
-        # return False
+        if (getenv('RPI_DEVICE') == 'false'):
+            return True
+        else:
+            result = run_command('sudo cat /etc/shadow | grep -w ' + username)
+            if result:
+                crypt = __import__('crypt').crypt
+                cryptedpasswd = result.split(':')[1]
+                is_correct = compare_hash(crypt(
+                    cleartext, cryptedpasswd), cryptedpasswd)
+                if is_correct:
+                    return True
+            return False
